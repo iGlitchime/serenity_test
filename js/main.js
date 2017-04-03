f924694969ec43b06ae36a12d99ff0bd693bf200
Irina Tsibizova <xsolarxwhisperx@gmail.com>
2017-04-03 20:05:51 +0300

    all desktop and first mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 /**
  * Created by IATS on 02.04.17.
  */
+
+//$(document).ready(function(){
+//    $('a[href*=#]').bind("click", function(e){
+//        var anchor = $(this);
+//        $('html, body').stop().animate({
+//            scrollTop: $(anchor.attr('href')).offset().top
+//        }, 1000);
+//        e.preventDefault();
+//    });
+//    return false;
+//});
+
 $("#myCarousel").carousel();
+$("#xsCarousel").carousel(); //сократить загрузку, если не девайс
+
+/* ---- begin change block ---- */
+$(document).ready(function(){
+    $(".info-more").click(function(){
+        var block = $(this).parent().parent();
+        block.find('> div').toggleClass('active').toggleClass('hidden');
+    });
+});
