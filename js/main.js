f5914df912a7909720e77ef419a00086cfd87a8a
Irina Tsibizova <xsolarxwhisperx@gmail.com>
2017-04-07 17:53:15 +0300

    ready

--- js/main.js
+++ js/main.js
@@ -5,24 +5,33 @@
 //$(document).ready(function(){
 //    $('a[href*=#]').bind("click", function(e){
 //        var anchor = $(this);
+//        var offsetTop = $(anchor.attr('href')).offset().top;
 //        $('html, body').stop().animate({
-//            scrollTop: $(anchor.attr('href')).offset().top
+//            scrollTop: offsetTop
 //        }, 1000);
 //        e.preventDefault();
 //    });
 //    return false;
 //});
 
-//$("#myCarousel").carousel();
-//$("#xsCarousel").carousel(); //сократить загрузку, если не девайс
+$(document).ready(function() {
+
+    var page2 = document.getElementById('page-2');
+    $('a').click(function(){
+        $('body').animate({scrollTop:page2.offsetTop}, 'slow');
+        return false;
+    });
 
 
-
-
-/* ---- begin change block ---- */
-$(document).ready(function(){
     $(".info-more").click(function(){
         var block = $(this).parent().parent();
         block.find('> div').toggleClass('active').toggleClass('hidden');
     });
-});
+
+    $("#mobMenu, #closeMenu").click(function(){
+        $('#menuFullscreen').toggleClass('hide');
+    });
+
+
+
+})
