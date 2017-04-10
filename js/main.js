7fc58ea32f00c300e8ee687d3a95963a09b7e3fc
Irina Tsibizova <xsolarxwhisperx@gmail.com>
2017-04-10 12:10:41 +0300

    left: animations

--- js/main.js
+++ js/main.js
@@ -2,25 +2,6 @@
  * Created by IATS on 02.04.17.
  */
 
-
-//var menuFullscreen = document.getElementById('menuFullscreen');
-//var mainPage = document.getElementById('page1');
-//
-//document.getElementById("mobMenu").addEventListener("click", function(){
-//
-//    menuFullscreen.style.opacity = "1";
-//    mainPage.style.opacity = "0";
-//
-//});
-//
-//document.getElementById("closeMenu").addEventListener("click", function(){
-//
-//    menuFullscreen.style.opacity = "0";
-//    mainPage.style.opacity = "1";
-//
-//});
-
-
 $(document).ready(function() {
 //
     var page2 = document.getElementById('page2');
@@ -35,26 +16,25 @@ $(document).ready(function() {
         block.find('> div').toggleClass('active').toggleClass('hidden');
     });
 
-//    $("#mobMenu, #closeMenu").click(function(){
-//        var menuFullscreen = document.getElementById('menuFullscreen');
-//        var mainPage = document.getElementById('#page1');
-//
-//        if(menuFullscreen.style.opacity == 0){
-//            menuFullscreen.style.opacity = "1";
-//            mainPage.style.opacity = "0";
-//
-//            menuFullscreen.style.left = "0";
-//            mainPage.style.right = "-" + window.innerWidth + "px";
-//
-//        } else{
-//            menuFullscreen.style.opacity = "0";
-//            menuFullscreen.style.left = "-" + window.innerWidth + "px";
-//            mainPage.style.opacity = "1";
-//            mainPage.style.right = "0";
-//        }
-//        //$('#menuFullscreen').toggleClass('hide');
-//    });
-//
+
+    $("#mobMenu, #closeMenu").click(function(){
+        var menuFullscreen = document.getElementById('menuFullscreen');
+
+        if(menuFullscreen.style.opacity == 0){
+            menuFullscreen.style.opacity = "1";
+            menuFullscreen.style.left = "0";
+
+        } else{
+            menuFullscreen.style.opacity = "0";
+            menuFullscreen.style.left = "-" + window.innerWidth + "px";
+        }
+    });
 
 });
+
+
+
+
+
+
 
